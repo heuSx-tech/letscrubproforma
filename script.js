4495699f4372e9f41492173990f4d55bdeb5d399
heuSx-tech <heusxcsgo@gmail.com>
2025-05-03 02:47:23 +0300

    script.js

--- script.js
+++ script.js
@@ -3,38 +3,38 @@ let manuelFiyatGirildi = false;
 
 // Ürünlerin GTIN numaralarına göre fiyatları
 const urunFiyatlari = {
-  "8684378716026": { "1000-5000": 3.60, "5000-10000": 3.40, "10000+": 3.20 },
-"8684378716033": { "1000-5000": 3.60, "5000-10000": 3.40, "10000+": 3.20 },
-"8684378716040": { "1000-5000": 3.60, "5000-10000": 3.40, "10000+": 3.20 },
-"8684378716057": { "1000-5000": 3.60, "5000-10000": 3.40, "10000+": 3.20 },
-"8684378716071": { "1000-5000": 3.60, "5000-10000": 3.40, "10000+": 3.20 },
-"8684378716378": { "1000-5000": 3.60, "5000-10000": 3.40, "10000+": 3.20 },
-"8684378716385": { "1000-5000": 3.60, "5000-10000": 3.40, "10000+": 3.20 },
-"8684378716095": { "1000-5000": 2.40, "5000-10000": 2.20, "10000+": 1.90 },
-"8684378716132": { "1000-5000": 6.50, "5000-10000": 6.40, "10000+": 6.20 },
-"8684378716309": { "1000-5000": 6.50, "5000-10000": 6.40, "10000+": 6.20 },
-"8684378716323": { "1000-5000": 6.00, "5000-10000": 5.80, "10000+": 5.60 },
-"8684378716149": { "1000-5000": 3.00, "5000-10000": 2.80, "10000+": 2.60 },
-"8684378716156": { "1000-5000": 3.00, "5000-10000": 2.80, "10000+": 2.60 },
-"8684378716163": { "1000-5000": 3.00, "5000-10000": 2.80, "10000+": 2.60 },
-"8684378716170": { "1000-5000": 3.00, "5000-10000": 2.80, "10000+": 2.60 },
-"8684378716224": { "1000-5000": 1.90, "5000-10000": 1.80, "10000+": 1.70 },
-"8684378716231": { "1000-5000": 1.90, "5000-10000": 1.80, "10000+": 1.70 },
-"8684378716248": { "1000-5000": 1.90, "5000-10000": 1.80, "10000+": 1.70 },
-"8684378716255": { "1000-5000": 1.90, "5000-10000": 1.80, "10000+": 1.70 },
-"8684378716262": { "1000-5000": 1.90, "5000-10000": 1.80, "10000+": 1.70 },
-"8684378716279": { "1000-5000": 1.90, "5000-10000": 1.80, "10000+": 1.70 },
-"8684378716392": { "1000-5000": 1.70, "5000-10000": 1.60, "10000+": 1.50 },
-"8684378716408": { "1000-5000": 1.70, "5000-10000": 1.60, "10000+": 1.50 },
-"8684378716415": { "1000-5000": 1.70, "5000-10000": 1.60, "10000+": 1.50 },
-"8684378716422": { "1000-5000": 1.70, "5000-10000": 1.60, "10000+": 1.50 },
-"8684378716286": { "1000-5000": 4.30, "5000-10000": 4.10, "10000+": 3.90 },
-"8684378716293": { "1000-5000": 4.00, "5000-10000": 3.80, "10000+": 3.60 },
-"8684378716330": { "1000-5000": 3.20, "5000-10000": 3.10, "10000+": 3.00 },
-"8684378716347": { "1000-5000": 4.00, "5000-10000": 3.80, "10000+": 3.60 },
-"8684378716354": { "1000-5000": 4.00, "5000-10000": 3.80, "10000+": 3.60 },
-"8684378716361": { "1000-5000": 4.40, "5000-10000": 4.20, "10000+": 4.00 },
-"8684378716439": { "1000-5000": 0.70, "5000-10000": 0.55, "10000+": 0.40 },
+  "8684378716026 - DEEP EXFOLIATING MITT - ECRU -  FOR NORMAL SKIN": { "1000-5000": 3.60, "5000-10000": 3.40, "10000+": 3.20 },
+"8684378716033 - DEEP EXFOLIATING MITT - BLACK - FOR NORMAL SKIN": { "1000-5000": 3.60, "5000-10000": 3.40, "10000+": 3.20 },
+"8684378716040 - DEEP EXFOLIATING MITT - TRİANGLE BLACK - FOR NORMAL SKIN": { "1000-5000": 3.60, "5000-10000": 3.40, "10000+": 3.20 },
+"8684378716057 - DEEP EXFOLIATING MITT - SALMON - FOR SENSITIVE SKIN": { "1000-5000": 3.60, "5000-10000": 3.40, "10000+": 3.20 },
+"8684378716071 - DEEP EXFOLIATING MITT - BEIGE - FOR SENSITIVE SKIN ": { "1000-5000": 3.60, "5000-10000": 3.40, "10000+": 3.20 },
+"8684378716378 - PREMIUM DEEP EXFOLIATING MITT - ORANGE - FOR NORMAL SKIN": { "1000-5000": 3.60, "5000-10000": 3.40, "10000+": 3.20 },
+"8684378716385 - PREMIUM DEEP EXFOLIATING MITT - GREY - FOR NORMAL SKIN": { "1000-5000": 3.60, "5000-10000": 3.40, "10000+": 3.20 },
+"8684378716095 - DEEP EXFOLIATING FACE MITT - SALMON - FOR ALL SKIN": { "1000-5000": 2.40, "5000-10000": 2.20, "10000+": 1.90 },
+"8684378716132 - DEEP EXFOLIATING BACK MITT - WHITE - FOR ALL SKIN": { "1000-5000": 6.50, "5000-10000": 6.40, "10000+": 6.20 },
+"8684378716309 - DEEP EXFOLIATING BACK MITT - BLACK - FOR ALL SKIN": { "1000-5000": 6.50, "5000-10000": 6.40, "10000+": 6.20 },
+"8684378716323 - NATURAL HORSE HAIR ANTI- CELLULITE BRUSH": { "1000-5000": 6.00, "5000-10000": 5.80, "10000+": 5.60 },
+"8684378716149 - EXFOLIATING WASHCLOTH - ORANGE": { "1000-5000": 3.00, "5000-10000": 2.80, "10000+": 2.60 },
+"8684378716156 - EXFOLIATING WASHCLOTH - GREEN": { "1000-5000": 3.00, "5000-10000": 2.80, "10000+": 2.60 },
+"8684378716163 - EXFOLIATING WASHCLOTH - BLUE": { "1000-5000": 3.00, "5000-10000": 2.80, "10000+": 2.60 },
+"8684378716170 - EXFOLIATING WASHCLOTH - YELLOW": { "1000-5000": 3.00, "5000-10000": 2.80, "10000+": 2.60 },
+"8684378716224 - NATURAL LOOFAH SOAP BAR - ALOE VERA": { "1000-5000": 1.90, "5000-10000": 1.80, "10000+": 1.70 },
+"8684378716231 - NATURAL LOOFAH SOAP BAR - RICE": { "1000-5000": 1.90, "5000-10000": 1.80, "10000+": 1.70 },
+"8684378716248 - NATURAL LOOFAH SOAP BAR - COLLAGEN": { "1000-5000": 1.90, "5000-10000": 1.80, "10000+": 1.70 },
+"8684378716255 - NATURAL LOOFAH SOAP BAR - TEA TREE": { "1000-5000": 1.90, "5000-10000": 1.80, "10000+": 1.70 },
+"8684378716262 - NATURAL LOOFAH SOAP BAR - GOAT MILK": { "1000-5000": 1.90, "5000-10000": 1.80, "10000+": 1.70 },
+"8684378716279 - NATURAL LOOFAH SOAP BAR - HONEY AND MILK": { "1000-5000": 1.90, "5000-10000": 1.80, "10000+": 1.70 },
+"8684378716392 - NATURAL OLIVE OIL SOAP - ROSE OIL": { "1000-5000": 1.70, "5000-10000": 1.60, "10000+": 1.50 },
+"8684378716408 - NATURAL OLIVE OIL SOAP - LAVENDER OIL": { "1000-5000": 1.70, "5000-10000": 1.60, "10000+": 1.50 },
+"8684378716415 - NATURAL OLIVE OIL SOAP - SNAIL EXTRACT": { "1000-5000": 1.70, "5000-10000": 1.60, "10000+": 1.50 },
+"8684378716422 - NATURAL OLIVE OIL SOAP - SULFUR JUNIPER TAR": { "1000-5000": 1.70, "5000-10000": 1.60, "10000+": 1.50 },
+"8684378716286 - EXFOLIATING BODY SCRUB - ANTIOXIDANT": { "1000-5000": 4.30, "5000-10000": 4.10, "10000+": 3.90 },
+"8684378716293 - EXFOLIATING BODY SCRUB - COLD EFFECT": { "1000-5000": 4.00, "5000-10000": 3.80, "10000+": 3.60 },
+"8684378716330 - MOISTURIZING LIP EXFOLIATOR 30 GR": { "1000-5000": 3.20, "5000-10000": 3.10, "10000+": 3.00 },
+"8684378716347 - ENZYM POWDER PEELING": { "1000-5000": 4.00, "5000-10000": 3.80, "10000+": 3.60 },
+"8684378716354 - 24H SILKY BODY YOGURT - 250 ML": { "1000-5000": 4.00, "5000-10000": 3.80, "10000+": 3.60 },
+"8684378716361 - BAKUCHIOL SKINCARE OIL Retinol Alternative 100 ML": { "1000-5000": 4.40, "5000-10000": 4.20, "10000+": 4.00 },
+"8684378716439 - NATURAL PUMICE STONE	": { "1000-5000": 0.70, "5000-10000": 0.55, "10000+": 0.40 },
 	
 
   // Diğer ürünler eklenmeli
@@ -105,6 +105,7 @@ function urunEkle() {
 }
 
 function proformaOlustur() {
+  
   hesapla();
 
   const firmaAdi = document.getElementById("firmaAdi").value;
@@ -123,21 +124,43 @@ function proformaOlustur() {
       <style>
         body { font-family: Arial, sans-serif; padding: 20px; }
         table { border-collapse: collapse; width: 100%; margin-top: 20px; }
-        th, td { border: 1px solid #ccc; padding: 10px; text-align: center; }
+        th, td { border: 1px solid #ccc; padding: 1px; text-align: center; }
         th { background-color: #f0f0f0; }
         h2 { color: #f38379; }
       </style>
     </head>
     <body>
+     <div style="position: absolute; top: 10px; left: -10px;">
       <img src="chimicos logo-02.png" alt="CHIMICOS Logo" style="width: 300px;"/>
+      </div>
+      <div style="position: relative; top: 55px; right: -5px;">
       <h2>PROFORMA FATURA</h2>
-      <p><strong>Tarih:</strong> ${bugun}</p>
+      </div>
+     <div style="position: relative; top: -50px; right: -900px;">
+  <p><strong>Tarih:</strong> ${bugun}</p>
+</div>
+
       <p>
+       <strong>__________________________________________________________________________________________________</strong><br>
+       <br> <strong> UĞURLU GRUP İNTERNET MAĞAZACILIK<br>
+ SANAYİ VE TİCARET LİMİTED ŞİRKETİ</strong> <br>
+ <p class="footer">
+        Adres: ALAADDİNBEY MH.626.SK-SAM-1 PLAZA B BLOK  <br>
+         Bina
+ No:22 Kapı No:12 Nilüfer/ BURSA <br>
+ VKN: 8861004082
+ <br>Vergi Dairesi: ÇEKİRGE V.D
+ <p>
+       <strong>__________________________________________________________________________________________________</strong><br>
+      </p>
+      
         <strong>Firma:</strong> ${firmaAdi}<br>
-        <strong>Vergi No:</strong> ${vergino}<br>
         <strong>Adres:</strong> ${adres}<br>
+        <strong>Vergi No:</strong> ${vergino}<br>
         <strong>E-posta:</strong> ${eposta}<br>
-        <strong>Telefon:</strong> ${telefon}
+        <strong>Telefon:</strong> ${telefon}<br>
+
+        <strong>__________________________________________________________________________________________________</strong><br>
       </p>
 
       <table>
@@ -152,21 +175,43 @@ function proformaOlustur() {
           <tr>
             <td>${item.urun}</td>
             <td>${item.adet}</td>
-            <td>${item.birimFiyat} EUR</td>
-            <td>${item.birimFiyatIndirimli} EUR</td>
-            <td>${(item.adet * item.birimFiyatIndirimli).toFixed(2)} EUR</td>
+            <td>${item.birimFiyat} USD</td>
+            <td>${item.birimFiyatIndirimli} USD</td>
+            <td>${(item.adet * item.birimFiyatIndirimli).toFixed(2)} USD</td>
           </tr>
         `).join('')}
+        <tr>
+  <td colspan="4" style="text-align: right;"><strong>Genel Toplam</strong></td>
+  <td><strong>${urunler.reduce((acc, item) => acc + (item.adet * item.birimFiyatIndirimli), 0).toFixed(2)} USD</strong></td>
+</tr>
+
       </table>
 
-      <p><strong>Euro Kuru: ${euroKuru} ₺</strong></p>
+      <p><strong>Dolar Kuru:</strong> ${euroKuru} ₺</p>
+      <p><strong>Kdv / Tax: </strong>${kdvDurumu} </p>
 
-      <p class="footer">
-        <strong>Let'Scrub</strong> - İletişim: letscrub.com
+  <p class="footer">
+        <strong>LET'SCRUB</strong> - İletişim: letscrub.com / info@letscrub.com.tr
       </p>
-      <p class="footer">
-        <strong>Açıklama:</strong> Ürün ödemesi sipariş itibari ile %50 peşin, kalan miktar ürün teslimi esnasında yapılacaktır. Proforma 20 iş günü geçerlidir. Döviz kuru fatura tarihindeki TCMB Efektif Satış Kuru olacaktır.
-      </p>
+       <p class="footer">
+            <strong> Açıklama: </strong> Ürün ödemesi sipariş itibari ile %50 peşin, kalan miktar ürün teslimi esnasında yapılacaktır.  </p>
+            <p class="footer">
+            Proforma 20 iş günü geçerlidir.  Döviz kuru fatura tarihindeki TCMB Efektif Satış Kuru olacaktır.
+            <p class="footer">
+             <strong>Ürün teslimat adresi:</strong> UĞURLU GRUP İNTERNET MAĞAZACILIK SANAYİ VE TİCARET LİMİTED ŞİRKETİ  dir.  
+            <p class="footer">
+<strong>Banka Bilgileri:</strong>
+ UĞURLU GRUP İNTERNET MAĞAZACILIK SANAYİ VE TİCARET LİMİTED ŞİRKETİ
+ <p class="footer">
+ <strong>IBAN :</strong> TR11 0004 6011 7988 8000 0890 14
+ <p class="footer">
+ <strong>Akbank USD:</strong>
+ TR49 0004 6011 7900 1000 0897 76 
+<p class="footer">
+<strong>Akbank EUR:</strong>
+TR80 0004 6011 7903 6000 0897 74
+            </p>
+            
     </body>
   </html>
   `;
